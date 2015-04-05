75a437f71afd025c4ec1ed27127f2898bfd4d50d
mikealegria <mikel063093@gmail.com>
2015-04-05 17:31:52 -0500

    fix

--- lt.js
+++ lt.js
@@ -1,5 +1,6 @@
 var localtunnel = require('localtunnel');
 var request = require('request');
+var macHelper =require('getmac');
 
 var port=80;
 var SystemId="";
@@ -10,20 +11,30 @@ function callback(err,tunnel){
     		console.log(err);
     }else{
 		console.log("localtunnel is running on"+tunnel.url);
+    macHelper.getMac(function(e,macAddress){
+      if(e){
 
-		request.post({url:'http://ubitaxi-ubibus.rhcloud.com/api/register', 
-			form: {id:SystemId, address: tunnel.url}}, function(err,response,body){ 
-			 if (!err && response.statusCode == 200) {
-			 	console.log("ok responde");
-			 	console.log(response);
-    				console.log(body) 
-  				}else{
+      }else{
+        var values ={address:"OK", ip:tunnel.url, mac: macAddress};
+        console.log(values);
+      request.post({url:'http://micro-ubibus.rhcloud.com/api/auth_pi', 
+      form: values}, function(err,response,body){ 
+       if (!err && response.statusCode == 200) {
+        console.log("ok responde");
+          
+            console.log(body) 
+          }else{
 
-  					console.log(response);
-  					console.log("err statusCode "+response.statusCode);
-  					console.log(err);
-  				}
-		})
+            console.log(response);
+            console.log("err statusCode "+response.statusCode);
+            console.log(err);
+          }
+          });
+      }
+    });
+
+
+	
     }
 }
 
