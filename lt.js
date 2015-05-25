216f76f9c1b650f0cfd7e05112004d44005012ee
mikealegria <mikel063093@gmail.com>
2015-05-24 19:00:53 -0500

    add led matrix integration on node

--- lt.js
+++ lt.js
@@ -69,12 +69,12 @@ function rederOnDisplay(str){
 
             context.font = '12px Arial';
             context.fillStyle = '#FF0000';
-            context.strokeStyle = 'rgba(255,0,0,255)';
+            //context.strokeStyle = 'rgba(255,0,0,255)';
             context.beginPath();
             context.stroke();
             context.translate(20,-40);
             context.fillText(text, 0, 16);
-            console.log('<img src="' + context.toDataURL() + '" />');
+            console.log('<img src="' + canvas.toDataURL() + '" />');
             board.drawCanvas(context, width, height)    
 }
 
