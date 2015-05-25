7aedd145babc70f117d430a9d850fdda1b473ee5
mikealegria <mikel063093@gmail.com>
2015-05-24 19:39:33 -0500

    add led matrix integration on node

--- lt.js
+++ lt.js
@@ -82,12 +82,12 @@ function rederOnDisplay(str){
             if(x){
               x=false;
            // context.fillText(text, textXpos, 16);}
-         }
+              }
             else{
               x=true;
               //context.fillText("Mike", textXpos, 16);
             }
-            context.fillText(text, textXpos, 16);}
+            context.fillText(text, textXpos, 16);
             //context.translate(20,-40);
             // context.beginPath();
             // c
